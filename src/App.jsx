8733f161eefdb3c73179bdf02ffd8f3a2ec80888
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-10-16 15:13:52 +0530

    navbarRunning

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ function App() {
 
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar/>
       </section>
       <section>Parallax</section>
-      <section>Services</section>
-      <section>Parallax</section>
-      <section>Portfolio1</section>
+      <section id="Services">Services</section>
+      <section id="">Parallax</section>
+      <section id="Portfolio">Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.scss"
+import {motion} from "framer-motion";
+import Sidebar from '../sidebar/Sidebar';
 
 const Navbar = () => {
+
+
   return (
     <div className='navbar'>
-      
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+         initial={{opacity:0,scale:0.5}} 
+         animate={{opacity:1,scale:1}}
+         transition={{duration:0.5}}
+         className="author"
+         >Ashanur Hossain</motion.span>
+        <div className="social">
+          <a><img href="#" src="./facebook.png" alt="" /></a>
+          <a><img href="#" src="./instagram.png" alt="" /></a>
+          <a><img href="#" src="./youtube.png" alt="" /></a>
+        </div>
+      </div>
     </div>
   )
 }
